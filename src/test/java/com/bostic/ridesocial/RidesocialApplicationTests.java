a9fa380c591b14fe36db6ca1fcd88b63c5b8df9c
Matt <mbostic@skillstorm.com>
2022-11-04 04:06:58 -0600

    Created Dockerfile. Varified that I can run in a container.

---
+++ src/test/java/com/bostic/ridesocial/RidesocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bostic.ridesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidesocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
